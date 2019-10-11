7e969c9afc494e48e2a5e52a7679cd2aa68ed204
Sergi Martínez <sergi.martinez@scmspain.com>
2019-10-11 10:54:43 +0200

    initial node server

---
+++ hook/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+// will use this later to send requests
+const http = require('http')
+// import env variables
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+	res.status(200).send('Server is working.')
+})
+
+app.listen(port, () => {
+	console.log(`🌏 Server is running at http://localhost:${port}`)
+})
